618cbc17207f9e41ff4ef603139b5b3163f3ff51
vlad.galuska <daiana.iurescu@gmail.com>
2021-11-09 11:44:57 +0200

    added stock component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProiectFE';
+  title = 'Plutus';
+  constructor(private elementRef: ElementRef) {
+  }
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.backgroundColor = '#132137';
+  }
 }
